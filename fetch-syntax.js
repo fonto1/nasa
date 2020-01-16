91d1b049711789ea92d73d280baec61ab398b899
Nanna <nannakari@gmail.com>
2020-01-16 13:46:06 +0100

    Update fetch-syntax.js

--- fetch-syntax.js
+++ fetch-syntax.js
@@ -5,7 +5,7 @@
 let RoverName = "curiosity"; // we can do a switch here where case 1 = curiosity, case 2 = ??? etc
 let SolNumber = 1000;
 let pageNr = 1;
-let ApiKey = "whateverthekey";
+let ApiKey = "VgavwUC9voBguJRtX3UTeFOTIz8lAmsFfDMnTD4W";
 
 let ULR = `https://api.nasa.gov/mars-photos/api/v1/rovers/${RoverName}/photos?sol=${SolNumber}&page=${pageNr}&api-key=${ApiKey}`;
 // https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
