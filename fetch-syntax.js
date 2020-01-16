ec1fa7728ea275d1a6a845b7ed4d5083e3b3c37a
Nanna <nannakari@gmail.com>
2020-01-16 14:05:55 +0100

    fetch update with url string

--- fetch-syntax.js
+++ fetch-syntax.js
@@ -2,20 +2,21 @@
 
 // fetching a page of photos from curiosity:
 
-let RoverName = "curiosity"; // we can do a switch here where case 1 = curiosity, case 2 = ??? etc
+let RoverName = 'curiosity'; // we can do a switch here where case 1 = curiosity, case 2 = ??? etc
 let SolNumber = 1000;
-let pageNr = 1;
-let ApiKey = "VgavwUC9voBguJRtX3UTeFOTIz8lAmsFfDMnTD4W";
+let pageNr = 2;
+let ApiKey = 'VgavwUC9voBguJRtX3UTeFOTIz8lAmsFfDMnTD4W';
 
-let ULR = `https://api.nasa.gov/mars-photos/api/v1/rovers/${RoverName}/photos?sol=${SolNumber}&page=${pageNr}&api-key=${ApiKey}`;
-// https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY
+// let ULR = `https://api.nasa.gov/mars-photos/api/v1/rovers/${RoverName}/photos?sol=${SolNumber}&page=${pageNr}&api_key=${ApiKey}`;
+let url = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + RoverName + "/photos?sol=" + SolNumber +"&page=" + pageNr + "&api_key=" + ApiKey;
 
 
-fetch(URL)
+fetch(url)
   .then((response) => {
     return response.json();
   })
   .then((myJson) => {
     console.log(myJson);
-    let data = myJson;
+    // let data = myJson;
 });
+
